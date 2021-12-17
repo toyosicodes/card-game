f49824fed8b797a929078a0a09a0fce2f6796eb7
Toyosi Salami <omotoyosisalami00@gmail.com>
2021-12-17 16:08:34 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import SingleCard from "./components/SingleCard";
+
+const cardImages = [
+  { src: "/images/ari.jpg", matched: false },
+  { src: "/images/beyx.jpg", matched: false },
+  { src: "/images/doja.jpg", matched: false },
+  { src: "/images/lisa.jpg", matched: false },
+  { src: "/images/tay.jpg", matched: false },
+  { src: "/images/tems.jpg", matched: false },
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [cardOne, setCardOne] = useState(null);
+  const [cardTwo, setCardTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCardOne(null);
+    setCardTwo(null);
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  const handleChoice = (card) => {
+    cardOne ? setCardTwo(card) : setCardOne(card);
+  };
+
+  useEffect(() => {
+    if (cardOne && cardTwo) {
+      setDisabled(true);
+      if (cardOne.src === cardTwo.src) {
+        setCards((prevCards) =>
+          prevCards.map((card) => {
+            if (card.src === cardTwo.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          })
+        );
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
+      }
+    }
+  }, [cardOne, cardTwo]);
+  const resetTurn = () => {
+    setCardOne(null);
+    setCardTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+    setDisabled(false);
+  };
+
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard
+            handleChoice={handleChoice}
+            card={card}
+            key={card.id}
+            flipped={card === cardOne || card === cardTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
---
+++ src/components/SingleCard.js
@@ -0,0 +1,23 @@
+const SingleCard = ({ card, handleChoice, flipped, disabled }) => {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+
+  return (
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img className="front" src={card.src} alt="card front" />
+        <img
+          onClick={handleClick}
+          className="back"
+          src="/images/hearts.jpg"
+          alt="card back"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SingleCard;
